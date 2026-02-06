d0b17c9170a5d3e8d628f94e4dbba819d8a0f189
Didier Mathieu <dmathieu2007@gmail.com>
2026-02-06 11:34:50 -0500

    remise finale

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import salutationsRouter from './src/routes/salutations.route.js';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 3030;
 
 app.use(express.json());
 
