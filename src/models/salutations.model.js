58f04b766ef95506ec59f6b761e6b933e7c2f40b
Didier Mathieu <dmathieu2007@gmail.com>
2026-02-06 11:15:10 -0500

    modification du model pour utiliser la bd

--- src/models/salutations.model.js
+++ src/models/salutations.model.js
@@ -1,27 +1,46 @@
-const salutations = [
-    { code_langue : "fr", langue : "Français", message : "Bonjour le monde"},
-    { code_langue : "fr", langue : "Français", message : "Bon matin"},
-    { code_langue : "fr", langue : "Français", message : "Salut"},
-    { code_langue : "fr", langue : "Français", message : "Bonne nuit je vais travailler"},
-    { code_langue : "en", langue : "Anglais", message : "Hello world"},
-    { code_langue : "en", langue : "Anglais", message : "Good morning"},
-    { code_langue : "en", langue : "Anglais", message : "Hi"},
-    { code_langue : "en", langue : "Anglais", message : "Good night, i''m going to work"},
-    { code_langue : "es", langue : "Espagnol", message : "Hola Mundo"},
-    { code_langue : "es", langue : "Espagnol", message : "Buenos dias"},
-    { code_langue : "es", langue : "Espagnol", message : "Hola"},
-    { code_langue : "es", langue : "Espagnol", message : "Buenas noches me voy a trabajar"},
-    { code_langue : "de", langue : "Allemand", message : "Hallo Welt"},
-    { code_langue : "de", langue : "Allemand", message : "guten Morgen"},
-    { code_langue : "de", langue : "Allemand", message : "Hallo"},
-    { code_langue : "de", langue : "Allemand", message : "Gute Nacht, ich gehe zur Arbei"}
-];
+import pool from '../config/db.js';
 
-const ajouterSalutation = (nouvelleSalutation) => {
-    salutations.push(nouvelleSalutation);
+const ajouterSalutation = async (nouvelleSalutation) => {
+    const requete = `INSERT INTO salutations (code_langue, langue, message) VALUES (?, ?, ?)`;
+    const params = [nouvelleSalutation.code_langue, nouvelleSalutation.langue, nouvelleSalutation.message];
+    
+    try {
+        const [resultats] = await pool.query(requete, params);
+        return resultats ?? null;
+    } catch (erreur) {
+        console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
+        throw erreur;
+    }
+};
+
+const getListeSalutation = async () => {
+    const requete = `SELECT code_langue, langue, message FROM salutations`;
+
+    try {
+        const [resultats] = await pool.query(requete);
+        return resultats ?? null;
+    } catch (erreur) {
+        console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : 
+                    ${erreur.sqlMessage}`);
+        throw erreur;
+    }
+}
+
+const getSalutationsParLangue = async (codeLangue) => {
+    const requete = `SELECT code_langue, langue, message FROM salutations WHERE code_langue = ?`;
+    const params = [codeLangue];
+    
+    try {
+        const [resultats] = await pool.query(requete, params);
+        return resultats ?? null;
+    } catch (erreur) {
+        console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
+        throw erreur;
+    }
 };
 
 export { 
-    salutations,
-    ajouterSalutation
+    ajouterSalutation,
+    getListeSalutation,
+    getSalutationsParLangue
 };
